4f9cc2eabc28e0bffdbcc3ae63a8f946b31e09ee
Alperen Kapusuz <sefalperen21@gmail.com>
2022-12-11 15:26:59 +0300

    selectdropdown eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,94 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { cities } from "./assets/cities/cities";
+import SelectDropdown from "react-native-select-dropdown";
 
 const App = () => {
+  const [selectedCity, setSelectedCity] = useState();
+  const [selectedDistrict, setSelectedDistrict] = useState();
+  const [selectedNeighbourhood, setSelectedNeighbourhood] = useState();
+  const [pk, setPk] = useState(0);
+  const dataHandler = async (plaka) => {
+    await axios
+      .get(`https://api.ubilisim.com/postakodu/il/${plaka}`)
+      .then((response) => setSelectedCity(response.data))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    if (
+      selectedCity == null &&
+      selectedDistrict == null &&
+      selectedNeighbourhood == null
+    ) {
+      return;
+    }
+    let temp = selectedCity.postakodu.filter(
+      (x) => x.ilce == selectedDistrict && x.mahalle == selectedNeighbourhood
+    )[0].pk;
+    setPk(temp);
+  }, [selectedNeighbourhood]);
+
   return (
     <View>
-      <Text>App</Text>
+      <SelectDropdown
+        data={cities}
+        onSelect={(selectedItem) => {
+          dataHandler(selectedItem[0]);
+        }}
+        buttonTextAfterSelection={(selectedItem) => {
+          return selectedItem;
+        }}
+        rowTextForSelection={(item) => {
+          return item;
+        }}
+      />
+      {selectedCity != null && (
+        <View>
+          <SelectDropdown
+            data={Array.from(
+              new Set(selectedCity.postakodu.map((x) => x.ilce))
+            )}
+            onSelect={(selectedItem) => {
+              setSelectedDistrict(selectedItem);
+            }}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              return item;
+            }}
+          />
+        </View>
+      )}
+      {selectedDistrict != null && (
+        <View>
+          <SelectDropdown
+            data={Array.from(
+              new Set(
+                selectedCity.postakodu
+                  .filter((x) => x.ilce == selectedDistrict)
+                  .map((x) => x.mahalle)
+              )
+            )}
+            onSelect={(selectedItem, index) => {
+              setSelectedNeighbourhood(selectedItem);
+            }}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              return `${item}`;
+            }}
+          />
+        </View>
+      )}
+      {pk != 0 && (
+        <Text>
+          {`${selectedCity.postakodu[0].il} ${selectedDistrict} ${selectedNeighbourhood} ${pk}`}
+        </Text>
+      )}
     </View>
   );
 };
